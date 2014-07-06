d70e77614f3d2c8c52c1c2f18776786ff137a4a0
Michael Mior <michael.mior@gmail.com>
2014-07-06 16:43:25 -0400

    Random cleanup

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,8 @@
 # GRC colorizes nifty unix tools all over the place
-if $(grc &>/dev/null) && ! $(brew &>/dev/null)
-then
-  source `brew --prefix`/etc/grc.bashrc
+if [ `uname` = "Darwin" ]
+  then
+  if $(grc &>/dev/null) && ! $(brew &>/dev/null)
+  then
+    source `brew --prefix`/etc/grc.bashrc
+  fi
 fi
