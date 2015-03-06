67fb918e19bbef79ad7b1fdefaf22986251321c8
Michael Mior <mmior@uwaterloo.ca>
2015-03-06 07:35:02 -0500

    Fix pyenv installation script

--- python/install.zsh
+++ python/install.zsh
@@ -1,6 +1,3 @@
-install_files 'dotfiles' "`find $DOTFILES_ROOT -maxdepth 2 -name \*.symlink`" "$HOME/.\$source"
-install_files 'binaries' "`find $DOTFILES_ROOT -mindepth 2 -maxdepth 3 -path '*/bin/*'`" "$HOME/bin/\$source"
-
 pyenv install -s 2.7.6
 pyenv install -s 3.4.0
 pyenv global 2.7.6
