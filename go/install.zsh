f9cc79e853478a835ecb85ca1902110a559e74ae
Michael Mior <michael.mior@gmail.com>
2015-07-06 21:19:32 -0400

    Fix go install script

--- go/install.zsh
+++ go/install.zsh
@@ -2,6 +2,6 @@ if [ ! -d ~/.gvm ]; then
     GVM_NO_GIT_BAK=1
     GVM_NO_UPDATE_PROFILE=1
     SRC_REPO=$DOTFILES_ROOT/go/gvm $DOTFILES_ROOT/go/gvm/binscripts/gvm-installer
-    gvm version > /dev/null 2>&1 || source /home/mmior/.gvm/scripts/gvm
-    gvm install release
+    gvm version > /dev/null 2>&1 || source $HOME/.gvm/scripts/gvm
+    gvm install go1.4
 fi
