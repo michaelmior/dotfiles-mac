341c4878eb07094911d3ac4fafa6e3ee19816538
Michael Mior <michael.mior@gmail.com>
2015-07-06 20:50:27 -0400

    Allow env variables for setting git info

--- git/install.zsh
+++ git/install.zsh
@@ -8,10 +8,19 @@ then
         git_credential='osxkeychain'
     fi
 
-    user ' - What is your github author name?'
-    read -e git_authorname
-    user ' - What is your github author email?'
-    read -e git_authoremail
+    git_authorname=$GIT_AUTHOR_NAME
+    if [ -z "$GIT_AUTHOR_NAME" ]
+    then
+        user ' - What is your github author name?'
+        read -e git_authorname
+    fi
+
+    git_authoremail=$GIT_AUTHOR_EMAIL
+    if [ -z "$GIT_AUTHOR_EMAIL" ]
+    then
+        user ' - What is your github author email?'
+        read -e git_authoremail
+    fi
 
     sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.symlink.example > git/gitconfig.symlink
 
