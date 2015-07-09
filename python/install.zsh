5ef4f090017c7a2d7a0389aa4cf49fdde2bd5d45
Michael Mior <mmior@uwaterloo.ca>
2015-07-08 22:12:37 -0400

    Fix Python install script

--- python/install.zsh
+++ python/install.zsh
@@ -1,6 +1,13 @@
-pyenv install -s 2.7.8
-pyenv install -s 3.4.0
-pyenv global 2.7.8
+source python/path.zsh
+eval "$(pyenv init -)"
 
-pip-app cqlsh
-pip-app csvkit
+pyenv install -s 2.7.9
+pyenv install -s 3.4.3
+pyenv global 2.7.9
+
+pip install virtualenv
+pip install pipsi
+pyenv rehash
+
+pipsi --bin-dir=$HOME/bin install cqlsh
+pipsi --bin-dir=$HOME/bin install csvkit
