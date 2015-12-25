4c59dc26856fa08901ec381a6a195f928788d04a
Michael Mior <michael.mior@gmail.com>
2015-12-25 07:22:24 -0500

    Update go version and "use" without gvm

--- go/gvm.zsh
+++ go/gvm.zsh
@@ -2,3 +2,4 @@ gvm() {
     source "$HOME/.gvm/scripts/gvm"
     gvm "$@"
 }
+. "$HOME/.gvm/environments/go1.5.2"
--- go/install.zsh
+++ go/install.zsh
@@ -3,5 +3,5 @@ if [ ! -d ~/.gvm ]; then
     GVM_NO_UPDATE_PROFILE=1
     SRC_REPO=$DOTFILES_ROOT/go/gvm $DOTFILES_ROOT/go/gvm/binscripts/gvm-installer
     gvm version > /dev/null 2>&1 || source $HOME/.gvm/scripts/gvm
-    gvm install go1.4
+    gvm install go1.5.2
 fi
