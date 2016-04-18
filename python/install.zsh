0aaf3b2d84ae79ca76840a25836e8a0afbbf1d3c
Michael Mior <michael.mior@gmail.com>
2016-04-17 22:30:22 -0400

    Add shyaml

--- python/install.zsh
+++ python/install.zsh
@@ -14,3 +14,4 @@ pyenv rehash
 [[ -d ~/.local/venvs/httpie ]] || pipsi --bin-dir=$HOME/bin install httpie
 [[ -d ~/.local/venvs/pip-tools ]] || pipsi --bin-dir=$HOME/bin install pip-tools
 [[ -d ~/.local/venvs/pygments ]] || pipsi --bin-dir=$HOME/bin install Pygments
+[[ -d ~/.local/venvs/shyaml ]] || pipsi --bin-dir=$HOME/bin install shyaml
