9b7d89e2594a1d25bb119dda756087c4a1001fcf
Michael Mior <michael.mior@gmail.com>
2016-04-17 22:30:14 -0400

    Fix node package installation

--- node/install.zsh
+++ node/install.zsh
@@ -3,5 +3,5 @@ nvm install v5.10.1
 nvm alias default $(nvm current)
 
 npm set progress=false
-(cd $HOME/.dotfiles/node && npm install -g)
+(cd $HOME/.dotfiles/node && npm install)
 npm set progress=true
