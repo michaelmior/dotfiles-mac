bc823e8a00c4149b702f9e24b48e4805da9a4514
Michael Mior <mmior@uwaterloo.ca>
2016-04-29 12:02:39 -0400

    Update Ruby versions

--- ruby/install.zsh
+++ ruby/install.zsh
@@ -1,6 +1,6 @@
 if [ ! -x ~/.rvm/scripts/rvm ]; then
     ./ruby/rvm-installer
     source ~/.rvm/scripts/rvm
-    rvm install 1.9.3,2.1
-    rvm --default use 2.1
+    rvm install 2.3
+    rvm --default use 2.3
 fi
