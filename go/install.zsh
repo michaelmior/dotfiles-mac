4478a8e31b65fc43fb38a061947628115687d53a
Michael Mior <michael.mior@gmail.com>
2016-05-20 10:04:38 +0300

    Update go version

--- go/install.zsh
+++ go/install.zsh
@@ -5,7 +5,7 @@ if [ ! -d ~/.gvm ]; then
     gvm version > /dev/null 2>&1 || source $HOME/.gvm/scripts/gvm
     gvm install go1.4.3 --prefer-binary
     gvm use go1.4.3
-    gvm install go1.5.2 --prefer-binary
+    gvm install go1.6.2 --prefer-binary
 fi
 
 go get github.com/ericchiang/pup
