6e8640d7c0ad0935de2dccaaa474e84de2ca269e
Michael Mior <mmior@uwaterloo.ca>
2016-09-07 19:11:31 -0400

    Update rvm install script

--- ruby/install.zsh
+++ ruby/install.zsh
@@ -1,4 +1,5 @@
 if [ ! -x ~/.rvm/scripts/rvm ]; then
+    gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     ./ruby/rvm-installer
     source ~/.rvm/scripts/rvm
     rvm install --quiet-curl 2.3
