f356f48cca6bf2aa2cd620859f95feb3a307f141
Michael Mior <michael.mior@gmail.com>
2017-01-31 08:12:06 -0500

    Fix node install script

--- node/install.zsh
+++ node/install.zsh
@@ -1,5 +1,11 @@
 source $HOME/.nvm/nvm.sh
+
+if nvm alias default 2> /dev/null | grep -vq none; then
+  nvm use --delete-prefix default
+fi
+
 nvm install v7.4.0 > /dev/null
+nvm use --delete-prefix v7.4.0
 nvm alias default $(nvm current)
 npm config delete prefix
 
