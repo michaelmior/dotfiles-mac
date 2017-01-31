8775eb15ae5707252d4ba30184720aa66b0cdb70
Michael Mior <michael.mior@gmail.com>
2017-01-31 07:53:19 -0500

    Update node.js

--- node/install.zsh
+++ node/install.zsh
@@ -1,6 +1,7 @@
 source $HOME/.nvm/nvm.sh
-nvm install v6.4.0 > /dev/null
+nvm install v7.4.0 > /dev/null
 nvm alias default $(nvm current)
+npm config delete prefix
 
 npm set progress=false
 (cd $HOME/.dotfiles/node && npm install --loglevel=warn)
