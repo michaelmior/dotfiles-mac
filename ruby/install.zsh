24999e6ba8cf1afc3d9d7d00e99e50a2bb765048
Michael Mior <michael.mior@gmail.com>
2017-02-23 19:37:44 -0500

    Upgrade default Ruby to v2.4

--- ruby/install.zsh
+++ ruby/install.zsh
@@ -2,8 +2,8 @@ if [ ! -x ~/.rvm/scripts/rvm ]; then
     gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     ./ruby/rvm-installer
     source ~/.rvm/scripts/rvm
-    rvm install --quiet-curl 2.3
-    rvm --default use 2.3
+    rvm install --quiet-curl 2.4
+    rvm --default use 2.4
 fi
 
 gem install -q \
