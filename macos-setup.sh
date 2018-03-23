16b6daab3ab0367e9ff5edb8632afa41afc255de
Michael Mior <mmior@uwaterloo.ca>
2018-03-23 08:01:17 -0400

    Try testing on macOS on Travis

---
+++ macos-setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+brew install \
+    bison \
+    editorconfig \
+    flex \
+    cabal-install \
+    curl \
+    dirmngr \
+    git \
+    gnupg2 \
+    jq \
+    opam \
+    openssl \
+    php \
+    python \
+    racket \
+    tmux \
+    vim \
+    wget \
+    zsh \
+    binutils \
+    diffutils
+
+brew install --with-default-names \
+    gnu-sed \
+    gnu-tar \
+    gnu-which \
+    coreutils \
+    findutils \
+    grep \
+    ed
+
+brew tap caskroom/cask
+
+brew cask install \
+    java \
+    mactex
